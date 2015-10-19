cb6a31bbebf4c38e3ede177f8850f1356a59b6dd
dsshim <daveshim@gmail.com>
2015-10-19 08:44:04 -0600

    blinker

--- blink.js
+++ blink.js
@@ -1,1 +1,17 @@
-// YOUR CODE GOES HERE
+$.fn.blink = function(delay) {
+  return this.each(function() {
+    var obj = $(this);
+    setInterval(function() {
+      if ($(obj).css("visibility") == "visible") {
+        $(obj).css('visibility', 'hidden');
+      }
+      else {
+        $(obj).css('visibility', 'visible');
+      }
+    }, delay);
+  });
+}
+
+jQuery('.myDiv').blink(1000);
+// twice as fast
+jQuery('.otherDiv').blink(500);// YOUR CODE GOES HERE
